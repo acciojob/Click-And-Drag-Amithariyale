1a95108357ddaec11208ef6bcd1dd7c96f87487b
Amit Hariyale <57282097+Amithariyale@users.noreply.github.com>
2024-01-03 16:32:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-// Your code here.
+var cubes = document.querySelectorAll(".item");
+
+for (var i = 0; i < cubes.length; i++) {
+  cubes[i].addEventListener("mousedown", function (event) {
+    console.log(this,event)
+    this.startX = event.clientX;
+    this.startY = event.clientY;
+  });
+
+  cubes[i].addEventListener("mousemove", function (event) {
+    if (this.startX) {
+      this.style.left = `${this.startX - event.clientX}px`;
+      this.style.top = `${this.startY - event.clientY}px`;
+    }
+  });
+
+  cubes[i].addEventListener("mouseup", function (event) {
+    this.startX = null;
+    this.startY = null;
+  });
+}
